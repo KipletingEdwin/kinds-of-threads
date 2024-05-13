3d89843737a11466f046ac097be415a8189e7520
Kipleting Edwin <kipletingedwin4@gmail.com>
2024-05-13 13:06:58 +0300

    finalize on threads

--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,17 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        // Deamon = a low priority thread that runs in the background to perform tasks such as garbage collection
+        // JVM terminates itself when all user threads (non-deamon threads) finish their execution
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        MyThread  thread2 = new MyThread();
+
+        thread2.setDaemon(false);
+        System.out.println(thread2.isDaemon());
+
+        thread2.start();
+
+
+
     }
 }
---
+++ src/MyThread.java
@@ -0,0 +1,12 @@
+public class MyThread extends  Thread{
+
+    @Override
+
+    public  void run() {
+        if(this.isDaemon()){
+            System.out.println("This is a Deamon thread that  is running");
+        } else {
+            System.out.println("This is a user thread that is running");
+        }
+    }
+}
